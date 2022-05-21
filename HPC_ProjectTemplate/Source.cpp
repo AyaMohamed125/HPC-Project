22f951dd95edcc4f257cb0a81d0de69668c8691b
yoka47961 <94013110+yoka47961@users.noreply.github.com>
2022-05-21 18:37:28 +0200

    update 10N

--- HPC_ProjectTemplate/Source.cpp
+++ HPC_ProjectTemplate/Source.cpp
@@ -81,7 +81,7 @@ void createImage(int* image, int width, int height, int index)
 		}
 	}
 	//MyNewImage.Save("..//Data//Output//outputRes" + index + ".png");
-	MyNewImage.Save("C:\\Users\\ayamo\\Documents\\GitHub\\HPC-Project\\Data\\OutPut" + index + ".png");
+	MyNewImage.Save("C:\\Users\\ayamo\\Documents\\GitHub\\HPC-Project\\Data\\OutPut\\10N\\" + index + ".png");
 	cout << "result Image Saved " << index << endl;
 }
 
@@ -96,142 +96,149 @@ int main()
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	System::String^ imagePath;
-	std::string img;
-	img = "C:\\Users\\ayamo\\Documents\\GitHub\\HPC-Project\\Data\\Input\\test.png";
-
-	imagePath = marshal_as<System::String^>(img);
-	int* imageData = inputImage(&ImageWidth, &ImageHeight, imagePath);
-
-	int* pixel_intenisties = new int[256]{ 0 };
-	double* probability = new double[256];
-	double* comProbability = new double[256]{ 0 };
-	int* floorComProbability = new int[256];
-	/////////////////////////////////////////////////////////
-	// parallel data
-	int P_pixel_intenistiesLength = (width * height) / size - 1;
-
-	//for step 1,2
-	double* P_pixel_intenisties = new double[P_pixel_intenistiesLength];
-	double* P_pixel_intenistiesArray = new double[256]{ 0 };
-	int* sendCount1 = new int[size] {P_pixel_intenistiesLength};
-	int* displs = new int[size] {0, P_pixel_intenistiesLength, P_pixel_intenistiesLength * 2, P_pixel_intenistiesLength * 3, P_pixel_intenistiesLength * 4, P_pixel_intenistiesLength * 5, P_pixel_intenistiesLength * 6, P_pixel_intenistiesLength * 7};
-	int reminder = (width * height) % size - 1;
-	sendCount1[size - 1] = reminder;
-
-	//for step 3
-	double* Final_P_pixel_intenistiesArray = new double[256]{ 0 };
-	double localSum = 0;
-	double* P_comutative_Probability = new double[256]{ 0 };
-
-	//for step 4 and 5 
-	double* Floor_P_comutative_Probability = new double[256]{ 0 };
-	double* FinalOutputsmall = new double[P_pixel_intenistiesLength] {0};
-	double* FinalOutput = new double[256]{ 0 };
-
-	start_s = clock();
-
-	/////////////////////////////////////////////////////////
-
-	//sequential code
-	if (size == 1)
+	for (int s = 1; s <= 10; s++)
 	{
-		if (rank == 0)
+		System::String^ imagePath;
+		std::string img;
+		img = "C:\\Users\\ayamo\\Documents\\GitHub\\HPC-Project\\Data\\Input\\10N\\"+to_string(s)+".png";
+
+		imagePath = marshal_as<System::String^>(img);
+		int* imageData = inputImage(&ImageWidth, &ImageHeight, imagePath);
+
+		int* pixel_intenisties = new int[256]{ 0 };
+		double* probability = new double[256];
+		double* comProbability = new double[256]{ 0 };
+		int* floorComProbability = new int[256];
+		/////////////////////////////////////////////////////////
+		// parallel data
+		int P_pixel_intenistiesLength = (width * height) / size - 1;
+
+		//for step 1,2
+		double* P_pixel_intenisties = new double[P_pixel_intenistiesLength];
+		double* P_pixel_intenistiesArray = new double[256]{ 0 };
+		int* sendCount1 = new int[size] {P_pixel_intenistiesLength};
+		int* displs = new int[size] {0, P_pixel_intenistiesLength, P_pixel_intenistiesLength * 2, P_pixel_intenistiesLength * 3, P_pixel_intenistiesLength * 4, P_pixel_intenistiesLength * 5, P_pixel_intenistiesLength * 6, P_pixel_intenistiesLength * 7};
+		int reminder = (width * height) % size - 1;
+		sendCount1[size - 1] = reminder;
+
+		//for step 3
+		double* Final_P_pixel_intenistiesArray = new double[256]{ 0 };
+		double localSum = 0;
+		double* P_comutative_Probability = new double[256]{ 0 };
+
+		//for step 4 and 5 
+		double* Floor_P_comutative_Probability = new double[256]{ 0 };
+		double* FinalOutputsmall = new double[P_pixel_intenistiesLength] {0};
+		double* FinalOutput = new double[256]{ 0 };
+
+		start_s = clock();
+
+		/////////////////////////////////////////////////////////
+
+		//sequential code
+		if (size == 1)
 		{
-			//step #1
-			for (int i = 0; i < width * height ; i++)
-				pixel_intenisties[imageData[i]]++;
-			
-			//step #2
-			for (int i = 0; i < sizeOfImage; i++)
-				probability[i] = (double)pixel_intenisties[i] / (double)(width * height);
-				
-			//step #3
-			double sum = 0;
-			for (int i = 0; i < sizeOfImage; i++)
+			if (rank == 0)
 			{
-				sum += probability[i];
-				comProbability[i] = sum;
+				//step #1
+				for (int i = 0; i < width * height; i++)
+					pixel_intenisties[imageData[i]]++;
+
+				//step #2
+				for (int i = 0; i < sizeOfImage; i++)
+					probability[i] = (double)pixel_intenisties[i] / (double)(width * height);
+
+				//step #3
+				double sum = 0;
+				for (int i = 0; i < sizeOfImage; i++)
+				{
+					sum += probability[i];
+					comProbability[i] = sum;
+				}
+
+				//step #4
+				for (int i = 0; i < sizeOfImage; i++)
+					floorComProbability[i] = floor(comProbability[i] * 256);
+
+				//step #5
+				for (int i = 0; i < width * height; i++)
+					imageData[i] = floorComProbability[imageData[i]];
 			}
 
-			//step #4
-			for (int i = 0; i < sizeOfImage; i++)
-				floorComProbability[i] = floor(comProbability[i] * 256);
-
-			//step #5
-			for (int i = 0; i < width * height; i++)
-				imageData[i] = floorComProbability[imageData[i]];
 		}
+		//parallel code////////////////////////////
+		else
+		{
+
+
+			//step 1
+			cout << "FFF\n";
+			MPI_Scatterv(imageData, sendCount1, displs, MPI_INT, P_pixel_intenisties, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+			//MPI_Scatter(imageData, P_pixel_intenistiesLength, MPI_DOUBLE, P_pixel_intenisties, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+			for (int i = 0; i < P_pixel_intenistiesLength; i++)
+			{
+				P_pixel_intenistiesArray[(int)P_pixel_intenisties[i]]++;
+			}
+			MPI_Reduce(P_pixel_intenistiesArray, Final_P_pixel_intenistiesArray, P_pixel_intenistiesLength, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+			if (rank == 0)
+			{
+				for (int i = 0; i < 256; i++)
+				{
+					cout << imageData[i] << " ";
+				}
+			}
+
+			//step 3
+			if (rank == 0)
+			{
+				for (int i = 0; i < 256; i++)
+				{
+					Final_P_pixel_intenistiesArray[i] /= (double)(width * height);
+				}
+				for (int i = 0; i < 256; i++)
+				{
+					localSum += Final_P_pixel_intenistiesArray[i];
+					P_comutative_Probability[i] += localSum;
+				}
+				for (int i = 0; i < 256; i++)
+				{
+					Floor_P_comutative_Probability[i] = floor(P_comutative_Probability[i] * 255);
+				}
+			}
+
+			//step 5 
+			MPI_Scatterv(imageData, sendCount1, displs, MPI_INT, FinalOutputsmall, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+			//MPI_Scatter(imageData, P_pixel_intenistiesLength, MPI_DOUBLE, FinalOutputsmall, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+			for (int i = 0; i < P_pixel_intenistiesLength; i++)
+			{
+				FinalOutputsmall[i] = Floor_P_comutative_Probability[(int)FinalOutputsmall[i]];
+			}
+			//MPI_Gather(FinalOutputsmall, P_pixel_intenistiesLength, MPI_DOUBLE, FinalOutput, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+			MPI_Gatherv(FinalOutputsmall, P_pixel_intenistiesLength, MPI_DOUBLE, FinalOutput, sendCount1, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+			if (rank == 0)
+			{
+				createImage(imageData, ImageWidth, ImageHeight, 0);
+			}
+		}//HPC_ProjectTemplate
+
+		
+		////////////////////////////////////////////////////////
+
+		
+		start_s = clock();
+		createImage(imageData, ImageWidth, ImageHeight, s);
+		stop_s = clock();
+		TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
+		cout << "time: " << TotalTime << endl;
+		free(imageData);
 
 	}
-	//parallel code////////////////////////////
-	else
-	{
-
-
-		//step 1
-		cout << "FFF\n";
-		MPI_Scatterv(imageData, sendCount1, displs, MPI_INT, P_pixel_intenisties, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		//MPI_Scatter(imageData, P_pixel_intenistiesLength, MPI_DOUBLE, P_pixel_intenisties, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		for (int i = 0; i < P_pixel_intenistiesLength; i++)
-		{
-			P_pixel_intenistiesArray[(int)P_pixel_intenisties[i]]++;
-		}
-		MPI_Reduce(P_pixel_intenistiesArray, Final_P_pixel_intenistiesArray, P_pixel_intenistiesLength, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-		if (rank == 0)
-		{
-			for (int i = 0; i < 256; i++)
-			{
-				cout << imageData[i] << " ";
-			}
-		}
-
-		//step 3
-		if (rank == 0)
-		{
-			for (int i = 0; i < 256; i++)
-			{
-				Final_P_pixel_intenistiesArray[i] /= (double)(width * height);
-			}
-			for (int i = 0; i < 256; i++)
-			{
-				localSum += Final_P_pixel_intenistiesArray[i];
-				P_comutative_Probability[i] += localSum;
-			}
-			for (int i = 0; i < 256; i++)
-			{
-				Floor_P_comutative_Probability[i] = floor(P_comutative_Probability[i] * 255);
-			}
-		}
-
-		//step 5 
-		MPI_Scatterv(imageData, sendCount1, displs, MPI_INT, FinalOutputsmall, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		//MPI_Scatter(imageData, P_pixel_intenistiesLength, MPI_DOUBLE, FinalOutputsmall, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		for (int i = 0; i < P_pixel_intenistiesLength; i++)
-		{
-			FinalOutputsmall[i] = Floor_P_comutative_Probability[(int)FinalOutputsmall[i]];
-		}
-		//MPI_Gather(FinalOutputsmall, P_pixel_intenistiesLength, MPI_DOUBLE, FinalOutput, P_pixel_intenistiesLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		MPI_Gatherv(FinalOutputsmall, P_pixel_intenistiesLength, MPI_DOUBLE, FinalOutput, sendCount1, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-
-	}//HPC_ProjectTemplate
-
-	if (rank == 0)
-	{
-		createImage(imageData, ImageWidth, ImageHeight, 0);
-	}
-	////////////////////////////////////////////////////////
-
 	MPI_Finalize();
 	start_s = clock();
-	createImage(imageData, ImageWidth, ImageHeight, 0);
 	stop_s = clock();
 	TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
-	cout << "time: " << TotalTime << endl;
-
-	free(imageData);
-	
+	cout << "Total time: " << TotalTime << endl;
 
 	return 0;
 
